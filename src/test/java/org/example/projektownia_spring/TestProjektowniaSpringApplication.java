ccb6254e94c9854f953d2c9394c2b98fb1ca2908
TrebuszeQ <trebuszeq@gmail.com>
2024-03-19 20:31:53 +0100

    init

---
+++ src/test/java/org/example/projektownia_spring/TestProjektowniaSpringApplication.java
@@ -0,0 +1,30 @@
+package org.example.projektownia_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestProjektowniaSpringApplication {
+
+	@Bean
+	@ServiceConnection
+	ElasticsearchContainer elasticsearchContainer() {
+		return new ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"));
+	}
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(ProjektowniaSpringApplication::main).with(TestProjektowniaSpringApplication.class).run(args);
+	}
+
+}
